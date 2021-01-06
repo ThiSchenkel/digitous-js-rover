a9b38c6b88c0ba94eec274bac57df703a6b0b0e7
ThiSchenkel <thi.schenkel@gmail.com>
2021-01-06 17:33:06 +0100

    She is so amazing!!!

--- projet.js
+++ projet.js
@@ -67,25 +67,37 @@ console.log(rover); */
 
 function moveForward(rover) {
 
-    if (rover.direction === "N" && rover.y >= 0 && rover.y < 10) {
-        rover.y++;
-    } else if (rover.direction === "E" && rover.x >= 0 && rover.x < 10) {
-        rover.x++;
-    } else if (rover.direction === "S" && rover.y >= 0 && rover.y < 10) {
-        rover.y--;
-    } else if (rover.direction === "W" && rover.x >= 0 && rover.x < 10) {
-        rover.x--;
-    } else {
-        console.log("le mouvement impossible");
+    if (rover.direction === "N") {
+        if (rover.y > 0 && rover.y < 10) {
+            rover.y--;
+        } else { console.log("Le mouvement impossible") }
+
+    } else if (rover.direction === "E") {
+        if (rover.x >= 0 && rover.x < 10) {
+            rover.x++;
+        } else { console.log("Le mouvement impossible") }
+
+    } else if (rover.direction === "S") {
+        if (rover.y >= 0 && rover.y < 10) {
+            rover.y++;
+        } else { console.log("Le mouvement impossible") }
+
+    } else if (rover.direction === "W") {
+        if (rover.x >= 0 && rover.x < 10) {
+            rover.x--;
+        } else { console.log("Le mouvement impossible") }
     }
     return rover;
 }
 
-moveForward(rover)
-console.log(rover)
-moveForward(rover)
-console.log(rover)
+
 turnRight(rover)
+console.log(rover)
+moveForward(rover)
+console.log(rover)
+moveForward(rover)
+console.log(rover)
+turnLeft(rover)
 console.log(rover)
 moveForward(rover)
 console.log(rover)
