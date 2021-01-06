c8d519c4fab7184e124a2249a36b44c9b03e3c43
Ewa Kadziolka <kalistaewa@gmail.com>
2021-01-06 16:28:14 +0100

    3 functions

--- projet.js
+++ projet.js
@@ -23,25 +23,69 @@ var rover = {
 //les functions
 
 function turnRight(rover) {
-    rover.direction = "E";
-    console.log(rover.direction);
-    for (x = 0; x < 9; x++) {
-        if (x <= 0 || x <= 9) {
-            x++;
-        } else {
-            console.log("Error");
-        }
+    if (rover.direction === "N") {
+        rover.direction = "E"
+    } else if (rover.direction === "E") {
+        rover.direction = "S"
+    } else if (rover.direction === "S") {
+        rover.direction = "W"
+    } else {
+        rover.direction = "N"
     }
+    return rover;
 }
+/* turnRight(rover);
+console.log(rover);
 turnRight(rover);
 console.log(rover);
+turnRight(rover);
+console.log(rover);
+turnRight(rover);
+console.log(rover); */
 
-// function moveFoward(rover) {
-//     rover.direction = "S";
-//     console.log(rover.direction);
-// }
+function turnLeft(rover) {
+    if (rover.direction === "N") {
+        rover.direction = "W"
+    } else if (rover.direction === "W") {
+        rover.direction = "S"
+    } else if (rover.direction === "S") {
+        rover.direction = "E"
+    } else {
+        rover.direction = "N"
+    }
+    return rover;
+}
 
-// function turnLeft(rover) {
-//     rover.direction = "W";
-//     console.log(rover.direction);
-// }
+/* turnLeft(rover);
+console.log(rover);
+turnLeft(rover);
+console.log(rover);
+turnLeft(rover);
+console.log(rover);
+turnLeft(rover);
+console.log(rover); */
+
+function moveForward(rover) {
+
+    if (rover.direction === "N" && rover.y >= 0 && rover.y < 10) {
+        rover.y++;
+    } else if (rover.direction === "E" && rover.x >= 0 && rover.x < 10) {
+        rover.x++;
+    } else if (rover.direction === "S" && rover.y >= 0 && rover.y < 10) {
+        rover.y--;
+    } else if (rover.direction === "W" && rover.x >= 0 && rover.x < 10) {
+        rover.x--;
+    } else {
+        console.log("le mouvement impossible");
+    }
+    return rover;
+}
+
+moveForward(rover)
+console.log(rover)
+moveForward(rover)
+console.log(rover)
+turnRight(rover)
+console.log(rover)
+moveForward(rover)
+console.log(rover)
